42e9ee127e9c1809c57f03d7338eaeb67c035639
charulata871 <cln.charulata.871@gmail.com>
2026-01-01 21:03:27 +0530

    refactor: improve font variable naming in layout

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JJdbUA4ig/xw8uLWdNyMNC4LQA6xailuvGRdD2D0EAI=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_cfcba0db._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_e6b98469._.js",
-  "static/chunks/node_modules_aae0c16a._.js"
-],
-    source: "dynamic"
-});
